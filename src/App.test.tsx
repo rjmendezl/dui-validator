c941ebe2e2e6b6a14b25694bb2127b29b3e214b3
Ricardo Méndez <00103719@uca.edu.sv>
2022-10-21 13:06:12 -0600

    :memo: Update: App.test.tsx has been updated.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
 import { describe } from 'vitest'
+import {render, screen} from '@testing-library/react';
+import App from './App';
 
 describe('App', () => { 
-  it.todo('should render');
-  it.todo('should have title "Mi DUI es valido?"');
-  it.todo('should have an input text');
-  it.todo('should have a button with text "Validar"');
+  //it.todo('should render');
+  test('should render', () => 
+    {render(<App />);}
+  );
+
+  //it.todo('should have title "Mi DUI es valido?"');
+  test( 'should have title "Mi DUI es valido?"', () => {
+    render(<App />);
+    const title = screen.getByText('Mi DUI es valido?');
+    expect(title).toBeInTheDocument();
+  });
+
+  //it.todo('should have an input text');
+  test('should have an input text', () => {
+    render(<App />);
+    const input = screen.getByRole('textbox');
+    expect(input).toBeInTheDocument();
+  });
+
+  //it.todo('should have a button with text "Validar"');
+  test('should have a button with text "Validar"', () => {
+    render(<App />);
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+  });
+
   /* Lo que demas que se les ocurra que puedan probar */
+  // N/A
 })
